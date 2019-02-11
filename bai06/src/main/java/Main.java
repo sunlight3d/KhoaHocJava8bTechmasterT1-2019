84e062ae231b069156581185869ab0e3a5578e6c
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-02-11 21:08:59 +0700

    update

--- bai06/src/main/java/Main.java
+++ bai06/src/main/java/Main.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
@@ -6,5 +11,17 @@ public class Main {
         p1.playFootball();
         p1.goCamping();
         p1.readBook("Tay du ky");
+        Person p2 = new Person("Nam",
+                "nam@gmail.com", 26);
+        Person p3 = new Person("Quan",
+                "quan@gmail.com", 31);
+        List<Person> persons = new ArrayList<Person>();
+        persons.add(p1);
+        persons.add(p2);
+        persons.add(p3);
+        Collections.sort(persons);
+        for(Person p: persons) {
+            System.out.println(p.toString());
+        }
     }
 }
--- bai06/src/main/java/Person.java
+++ bai06/src/main/java/Person.java
@@ -1,7 +1,21 @@
-public class Person implements LearnAndStudy, Playable{
+public class Person implements LearnAndStudy, Playable, Comparable{
     private String name;
     private String email;
     private Integer age;
+
+    @Override
+    public String toString() {
+        return "Name: "+name+".Email:"+email+".Age:"+age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     //constructor
     public Person(String name, String email, Integer age) {
         this.name = name;
@@ -19,5 +33,22 @@ public class Person implements LearnAndStudy, Playable{
 
     public void goCamping() {
         System.out.println("Go camping");
+    }
+    /*
+    public int compareTo(Object o) {
+        Person a = (Person) o;
+        Person b = this;
+        if(a.getAge() > b.getAge()) {
+            return -1;
+        } else if (a.getAge() < b.getAge()){
+            return 1;
+        }
+        return 0;
+    }
+    */
+    public int compareTo(Object o) {
+        Person a = (Person) o;
+        Person b = this;
+        return -a.getName().compareTo(b.getName());
     }
 }
